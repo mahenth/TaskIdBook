6df37a12a0f4be8eebd972ed7cc32d20b659ebc5
mahenth <mahenth.v98@gmail.com>
2025-05-30 23:03:27 +0530

    updated

--- hotel_booking_project/settings.py
+++ hotel_booking_project/settings.py
@@ -102,13 +102,23 @@ WSGI_APPLICATION = 'hotel_booking_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',     
+        'USER': 'mydb',        
+        'PASSWORD': '123456', # Your database password
+        'HOST': 'localhost',               # Or the IP address/hostname of your PostgreSQL server
+        'PORT': '5432',                    # Default PostgreSQL port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
